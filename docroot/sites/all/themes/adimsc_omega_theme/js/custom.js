95639507191bb4732af6a88bfdf469d77a6f0810
Shazhad Anwar <shahzad.anwar@devtrongenesis.com>
2016-09-02 23:19:46 +0500

    Main menu

--- docroot/sites/all/themes/adimsc_omega_theme/js/custom.js
+++ docroot/sites/all/themes/adimsc_omega_theme/js/custom.js
@@ -2,5 +2,6 @@
 (function($) {
   $(document).ready(function() {
     $('#drop-down-login-wrapper.enable-dd .button span span').append('<i aria-hidden="true" class="fa fa-user"></i>');
+    $('.sf-main-menu ul').addClass('menu-dropdown').attr('style', 'margin-top: -4px');
   });
 })(jQuery);
