7857b117429963d1b214503321cef9576e37a497
Shazhad Anwar <shahzad.anwar@devtrongenesis.com>
2016-09-10 18:24:43 +0500

    Upcoming Event

--- docroot/sites/all/themes/adimsc_omega_theme/js/custom.js
+++ docroot/sites/all/themes/adimsc_omega_theme/js/custom.js
@@ -6,5 +6,10 @@
     $('.owl-next').append('<i aria-hidden="true" class="fa fa-angle-right"></i>');
     $('.owl-prev').append('<i aria-hidden="true" class="fa fa-angle-left"></i>');
     $('.views-content-title, .views-field-created').wrap('<div class="wrap-me">');
+    $('#block-views-cea8a95ddf9c6643ab310ee465277b95 div[id^="field-countdown-timer-note"]').remove();
+    $('#block-views-cea8a95ddf9c6643ab310ee465277b95 .countDays').append('<span class="counter-text count-days">Days</span>');
+    $('#block-views-cea8a95ddf9c6643ab310ee465277b95 .countHours').append('<span class="counter-text count-hours">Hours</span>');
+    $('#block-views-cea8a95ddf9c6643ab310ee465277b95 .countMinutes').append('<span class="counter-text count-minutes">Minutes</span>');
+    $('#block-views-cea8a95ddf9c6643ab310ee465277b95 .countSeconds').append('<span class="counter-text count-seconds`">Seconds</span>');
   });
 })(jQuery);
